633a63f6ffa373489473bdca8d880d1b53c616bd
luantn94 <41967138+luantn94@users.noreply.github.com>
2018-08-22 08:43:08 +0700

    update

--- project/src/scripts/main.js
+++ project/src/scripts/main.js
@@ -9,11 +9,26 @@ $(document).ready(function(){
 	});
 
 	$('.home-logo .owl-carousel').owlCarousel({
-		items: 5,
+		items: 2,
 		nav: true,
 		loop: true,
 		autoplay: true,
 		dots: false,
 		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		responsive: {
+			// breakpoint from 480 up
+			480: {
+				items: 2,
+			},
+			// breakpoint from 768 up
+			768: {
+				items: 3,
+			},
+			// breakpoint from 992 up
+			992: {
+				items: 5,
+			}
+		},
 	});
+
 });
